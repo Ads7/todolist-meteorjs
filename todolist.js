490d8f077495ff93f4433cc1038dde4e3645d3b0
Ads7 <ads71993@gmail.com>
2015-05-29 23:11:20 +0530

    auto pub removed

--- todolist.js
+++ todolist.js
@@ -1,7 +1,8 @@
 Todolists = new Mongo.Collection('todolists');
 
 if (Meteor.isClient) {
-  // counter starts at 0
+  Meteor.subscribe("todolists");
+
   Template.body.helpers({
     todolists: function(){
      if (Session.get('hideFinished')){
@@ -31,6 +32,11 @@ if (Meteor.isClient) {
     
 
     }
+  });
+  Template.todolist.helpers({
+    isOwner: function(){
+    return this.owner === Meteor.userId();
+    }
   });  
 Template.todolist.events({
   'click .toggle-checked':function(){
@@ -38,7 +44,10 @@ Template.todolist.events({
       },
 'click .delete': function(){
   Meteor.call("deleteTodolist",this._id);
-}
+},
+'click .toggle-private':function(){
+    Meteor.call("setPrivate",this._id, !this.private);  
+      }
 });
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
@@ -49,21 +58,48 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  Meteor.publish("todolists", function(){
+    return Todolists.find({
+      $or:[
+      {private: {$ne:true}},
+      {owner: this.userId}]
+    });
+
+  });
 }
 
 Meteor.methods({
   addTodolist:function(title){
      Todolists.insert({
         title: title,
-        createdAt: new Date()
-        
+        createdAt: new Date(),
+        owner:  Meteor.userId()
       });
   },
   updateTodolist: function(id, checked){
+    var lst = Todolists.findOne(id);
+
+    if(lst.owner !== Meteor.userId()){
+      throw new Meteor.Error('not authorized to update');
+    }
     Todolists.update(id, {$set:{checked: checked}});
 
   },
   deleteTodolist: function(id){
+    var lst = Todolists.findOne(id);
+
+    if(lst.owner !== Meteor.userId()){
+      throw new Meteor.Error('not authorized to delete');
+    }
     Todolists.remove(id);
+  },
+  setPrivate: function(id, private){
+    var lst = Todolists.findOne(id);
+
+    if(lst.owner !== Meteor.userId()){
+      throw new Meteor.Error('not authorized to set private');
+    }
+
+    Todolists.update(id, {$set:{private: private}});
   }
 });
